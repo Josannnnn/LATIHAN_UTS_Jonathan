f9c95351c10cc6c94ad6f22eced026a0fe8d1ed0
Kevin <>
2024-10-25 18:43:38 +0700

    Update stok.js

--- models/stok.js
+++ models/stok.js
@@ -1,30 +1,29 @@
 const mongoose = require("mongoose");
 
 //Create Collection Schema
-const pesanan = new mongoose.Schema({
+const stokSchema = new mongoose.Schema({
     produk_id: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pesanan',
+        ref: 'Stok',
         require: true,
     },
-    pengguna_id: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Pesanan',
-        require:true
-    },
     jumlah: {
-        type : Number,
+        type: Number,
+
     },
-    status: {
+    tanggal_update: {
+        type : Date,
+    },
+    keterangan: {
         type: String,
     },
-    tanggal_pesanan: {
-        type: Date,
+    lokasi: {
+        type: String,
     }
 });
 
 
 
 
-const pesanan = mongoose.model('Pesanan', pesananSchema);
-module.exports = pesanan;
+const stok = mongoose.model('Stok', stokSchema);
+module.exports = stok;
